1bd162e379ec01282e7bba1799bf01211d760e0c
Isaac Schiffler <ischiffl@calpoly.edu>
2024-04-09 18:01:28 -0700

    first iteration of ECB encryption; almost there, small bug somewhere...

--- main.py
+++ main.py
@@ -1,20 +1,61 @@
+import binascii
+import secrets
+
 from Crypto.Cipher import AES
-import secrets, binascii
 
 user_text = "sixteen sixteen :admin<true:"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
 def key_IV_gen():
     key = secrets.token_bytes(16)
-    return binascii.hexlify(key).decode()
+    return binascii.hexlify(key)
+
+
+# function to encrypt a 128 bit block and return the 128 bit block encryption
+def encrypt_block(block):
+    # block is a 128 bit block of a message we want encrypted
+
+    return
+
+
+# function to encrypt a file
+"""maybe pull out encryption of message to have encrypt_message 
+function and encrypt_file function that calls encrypt_message?"""
+def encrypt_file_ECB(filename, key):
+    with open(filename, 'rb') as f:
+        contents = f.read()
+
+    # save and remove header from bmp data
+    header = contents[:54]
+    contents = contents[54:]
+
+    # open cipher text file and write each block as encrypted
+    # change naming convention to customize for specified file we're encrypting at some point...
+    with open('cipher.bmp', 'ab') as f:
+        f.write(header)  # append header to encrypted file
+        cipher = AES.new(key, AES.MODE_ECB)
+        # iterate over every block
+        while len(contents) != 0:
+            # get blocks
+            block = contents[:16]
+            contents = contents[16:]
+            # check if padding is needed
+            if len(block) != 16:
+                bytes_needed = 16 - len(block)  # calculate number of bytes needing padding
+                # add padding
+                padding_byte = bytes([bytes_needed])
+                """what if the last block takes up 5.5 bytes, what of the padding for the .5 bytes
+                up to 6?????? Question for prof"""
+                block += padding_byte * bytes_needed  # adds the byte representation of the number of bytes padded
+
+            cipher_block = cipher.encrypt(block)  # encrypt block
+            f.write(cipher_block)  # write to cipher file
+
+    return
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    print(key_IV_gen())
+    print('PyCharm')
+    key = key_IV_gen()
+    encrypt_file_ECB('mustang.bmp', key)
