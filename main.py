4b9d8bfcc5dd37bc4defff84eb2cf9089bab405d
Isaac Schiffler <ischiffl@calpoly.edu>
2024-04-10 17:20:13 -0700

    combined encryption functions to one function where you can specify encryption mode

--- main.py
+++ main.py
@@ -8,44 +8,8 @@ user_text = "sixteen sixteen :admin<true:"
 # function to encrypt a file
 """maybe pull out encryption of message to have encrypt_message 
 function and encrypt_file function that calls encrypt_message?"""
-def encrypt_file_ECB(filename, key):
-    with open(filename, 'rb') as f:
-        contents = f.read()
-
-    # save and remove header from bmp data
-    header = contents[:54]
-    contents = contents[54:]
-
-    # Open the file in write mode ('w') to clear its contents
-    with open('cipherECB.bmp', 'w'):
-        pass
-
-    # open cipher text file and write each block as encrypted
-    # change naming convention to customize for specified file we're encrypting at some point...
-    with open('cipherECB.bmp', 'ab') as f:
-        f.write(header)  # append header to encrypted file
-        cipher = AES.new(key, AES.MODE_ECB)
-        # iterate over every block
-        while len(contents) != 0:
-            # get blocks
-            block = contents[:16]
-            contents = contents[16:]
-            # check if padding is needed
-            if len(block) != 16:
-                bytes_needed = 16 - len(block)  # calculate number of bytes needing padding
-                # add padding
-                padding_byte = bytes([bytes_needed])
-                """what if the last block takes up 5.5 bytes, what of the padding for the .5 bytes
-                up to 6?????? Question for prof"""
-                block += padding_byte * bytes_needed  # adds the byte representation of the number of bytes padded
-
-            cipher_block = cipher.encrypt(block)  # encrypt block
-            f.write(cipher_block)  # write to cipher file
-
-    return
-
-#function to encrypt a file using CBC block encryption... could combine this function with above to make more clean if we want...
-def encrypt_file_CBC(filename, key, IV):
+"""Function to encrypt a file. Supports ECB and CBC as its mode."""
+def encrypt_file(filename, key, IV, mode):
     scrambler = IV
     with open(filename, 'rb') as f:
         contents = f.read()
@@ -54,13 +18,18 @@ def encrypt_file_CBC(filename, key, IV):
     header = contents[:54]
     contents = contents[54:]
 
+    if mode == "CBC":
+        new_file_name = 'cipherCBC.bmp'
+    else:
+        new_file_name = 'cipherECB.bmp'
+
     # Open the file in write mode ('w') to clear its contents
-    with open('cipherCBC.bmp', 'w'):
+    with open(new_file_name, 'w'):
         pass
 
     # open cipher text file and write each block as encrypted
     # change naming convention to customize for specified file we're encrypting at some point...
-    with open('cipherCBC.bmp', 'ab') as f:
+    with open(new_file_name, 'ab') as f:
         f.write(header)  # append header to encrypted file
         cipher = AES.new(key, AES.MODE_ECB)
         # iterate over every block
@@ -74,14 +43,17 @@ def encrypt_file_CBC(filename, key, IV):
                 # add padding
                 padding_byte = bytes([bytes_needed])
                 block += padding_byte * bytes_needed  # adds the byte representation of the number of bytes padded
-            new = bytearray(len(block))
-            # xor with scrambler (init IV)
-            for i in range(0, len(block)):
-                new[i] = block[i] ^ scrambler[i]
+            if mode == "CBC":
+                new = bytearray(len(block))
+                # xor with scrambler (init IV)
+                for i in range(0, len(block)):
+                    new[i] = block[i] ^ scrambler[i]
+                block = new
             # encrypt new block
-            cipher_block = cipher.encrypt(new)
+            cipher_block = cipher.encrypt(block)
             f.write(cipher_block)
-            scrambler = cipher_block
+            if mode == "CBC":
+                scrambler = cipher_block
 
 
 # function to decrypt CBC to double check it works...
@@ -101,7 +73,28 @@ def decrypt_CBC(key, IV):
     unpadded_contents = unpad(decrypted_contents, AES.block_size)
 
     # Write the decrypted contents to a new BMP file
-    with open('decrypted.bmp', 'wb') as f:
+    with open('decryptedCBC.bmp', 'wb') as f:
+        f.write(header)
+        f.write(unpadded_contents)
+
+
+def decrypt_ECB(key):
+    with open('cipherECB.bmp', 'rb') as f:
+        encrypted_contents = f.read()
+
+    # Initialize AES cipher object with the key and IV
+    header = encrypted_contents[:54]
+    encrypted_contents = encrypted_contents[54:]
+    cipher = AES.new(key, AES.MODE_ECB)
+
+    # Decrypt the contents using AES in CBC mode
+    decrypted_contents = cipher.decrypt(encrypted_contents)
+
+    # Unpad the decrypted contents
+    unpadded_contents = unpad(decrypted_contents, AES.block_size)
+
+    # Write the decrypted contents to a new BMP file
+    with open('decryptedECB.bmp', 'wb') as f:
         f.write(header)
         f.write(unpadded_contents)
 
@@ -110,7 +103,8 @@ if __name__ == '__main__':
     print('PyCharm')
     key = get_random_bytes(16)
     IV = get_random_bytes(16)
-    encrypt_file_ECB('mustang.bmp', key)
-    encrypt_file_CBC('mustang.bmp', key, IV)
+    encrypt_file('mustang.bmp', key, "unneeded", "EBC")
+    encrypt_file('mustang.bmp', key, IV, "CBC")
 
     #decrypt_CBC(key, IV)
+    #decrypt_ECB(key)
